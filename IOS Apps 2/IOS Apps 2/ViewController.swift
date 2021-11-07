482486b563f0f406b62955a7bb008270bae2b57e
Udacoding Indonesia <78523253+udacodingid@users.noreply.github.com>
2021-11-07 13:52:12 +0700

    starter project

---
+++ IOS Apps 2/IOS Apps 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Apps 2
+//
+//  Created by IDOUDA on 07/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
